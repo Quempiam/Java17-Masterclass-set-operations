1391d5dbf27c36b57ee92368667c800a8bbe37c0
Quempiam <quempiam@gmail.com>
2024-03-05 00:11:47 +0100

    Release 1.0

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -7,11 +7,30 @@ public class Main {
     public static void main(String[] args) {
 
         Set<Task> tasks = TaskData.getTasks("all");
-        sortAndPrint("All Tasks", tasks);
+        sortAndPrint("Tasks from manager", tasks);
 
         Comparator<Task> sortByPriority = Comparator.comparing(Task::getPriority);
         Set<Task> annsTasks = TaskData.getTasks("Ann");
         sortAndPrint("Ann's Tasks", annsTasks, sortByPriority);
+
+        Set<Task> bobsTasks = TaskData.getTasks("Bob");
+        sortAndPrint("Bob's Tasks", bobsTasks);
+
+        Set<Task> carolsTasks = TaskData.getTasks("Carol");
+        sortAndPrint("Carol's Tasks", carolsTasks);
+
+        Set<Task> allTasks = getUnion(tasks, getUnion(annsTasks, getUnion(bobsTasks, carolsTasks)));
+        sortAndPrint("All Tasks", allTasks);
+
+        Set<Task> assignedTasks = getUnion(annsTasks, getUnion(bobsTasks, carolsTasks));
+        sortAndPrint("Tasks assigned to someone", allTasks);
+
+        Set<Task> notAssignedTasks = getDifference(getDifference(getDifference(tasks, annsTasks), bobsTasks), carolsTasks);
+        sortAndPrint("Not assigned tasks", notAssignedTasks, sortByPriority);
+
+        Set<Task> teamTasks = getIntersect(annsTasks, getIntersect(bobsTasks, carolsTasks));
+        sortAndPrint("Multiple assigned tasks", teamTasks, Comparator.comparing(Task::getStatus));
+
     }
 
     private static void sortAndPrint(String header, Collection<Task> collection) {
@@ -29,5 +48,23 @@ public class Main {
         List<Task> list = new ArrayList<>(collection);
         list.sort(sorter);
         list.forEach(System.out::println);
+    }
+
+    private static <T> Set<T> getUnion(Set<T> a, Set<T> b) {
+        Set<T> ret = new HashSet<>(a);
+        ret.addAll(b);
+        return ret;
+    }
+
+    private static <T> Set<T> getDifference (Set<T> a, Set<T> b){
+        Set<T> ret = new HashSet<>(a);
+        ret.removeAll(b);
+        return ret;
+    }
+
+    private static <T> Set<T> getIntersect (Set<T> a, Set<T> b){
+        Set<T> ret = new HashSet<>(a);
+        ret.retainAll(b);
+        return ret;
     }
 }
